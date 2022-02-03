c6ff88bb15a799e8672920fa6c0cd9b5415add56
Matt Edwards <edwardsjm01@gmail.com>
2022-02-02 18:15:16 -0600

    move to monorepo file structure

--- server/app.js
+++ server/app.js
@@ -14,9 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'client/dist/byu-disc-golf')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/course', courseRouter);
 
 // send frontend
