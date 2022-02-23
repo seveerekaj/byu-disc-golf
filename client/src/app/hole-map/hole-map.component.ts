8ff388f11119346938772463aa1b1d97c4718bbb
Matt Edwards <edwardsjm01@gmail.com>
2022-02-22 23:54:20 -0600

    position map correctly

--- client/src/app/hole-map/hole-map.component.ts
+++ client/src/app/hole-map/hole-map.component.ts
@@ -7,25 +7,40 @@ import { Goal } from '../goal';
   styleUrls: ['./hole-map.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HoleMapComponent implements OnChanges {
+export class HoleMapComponent implements OnInit, OnChanges {
   @ViewChild('discMap') googleMap!: google.maps.Map;
   @Input() hole!: Goal;
   mapOptions!: google.maps.MapOptions;
-  markers: google.maps.LatLngLiteral[] = [];
+  markers: google.maps.Marker[] = [];
+  screenHeight!: number;
+  screenWidth!:number;
   constructor() {
+  }
+  ngOnInit(): void {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const startMarker: google.maps.LatLngLiteral = { lat: this.hole.startLat, lng: this.hole.startLng };
-    const endMarker: google.maps.LatLngLiteral = { lat: this.hole.endLat, lng: this.hole.endLng };
+    // const startMarker: google.maps.LatLngLiteral = { lat: this.hole.startLat, lng: this.hole.startLng };
+    // const endMarker: google.maps.LatLngLiteral = { lat: this.hole.endLat, lng: this.hole.endLng };
+    const startMarker = new google.maps.Marker({
+      position: { lat: this.hole.startLat, lng: this.hole.startLng },
+      label: 'Start'
+    });
+    const endMarker = new google.maps.Marker({
+      position: { lat: this.hole.endLat, lng: this.hole.endLng },
+      label: 'End'
+    });
+
 
     this.markers.length = 0;
     this.markers.push(startMarker);
     this.markers.push(endMarker);
 
     const bounds = new google.maps.LatLngBounds();
-    bounds.extend(startMarker);
-    bounds.extend(endMarker);
+    bounds.extend(startMarker.getPosition()!);
+    bounds.extend(endMarker.getPosition()!);
 
     this.mapOptions = {
       center: bounds.getCenter(),
