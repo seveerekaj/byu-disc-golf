2a4f3bd18fd36bd2459d55d87e2289360f1e64b9
Connor Bunch <43152960+connor981332@users.noreply.github.com>
2022-03-07 19:11:42 -0700

    built up template front end api (#14)
    Co-authored-by: Connor Bunch <connorb@qualtrics.com>

--- server/bin/routers/response-templates.js
+++ server/bin/routers/response-templates.js
@@ -1,11 +1,29 @@
 "use strict";
 
-exports.success_template = {
-    "status" : "success",
-    "message" : ""
+const makeMsg = function(status, msg) {
+    if (!status) {
+        status = "failure"
+    }
+    if (!msg) {
+        msg = "";
+    }
+    return {
+        "status": status,
+        "message": msg
+    };
 }
 
-exports.failure_template = {
-    "status" : "failure",
-    "message" : ""
+const makeSuccessMsg = function(msg) {
+    return makeMsg("success", msg);
 }
+
+const makeFailureMsg = function(msg) {
+    return makeMsg("failure", msg);
+}
+
+module.exports = {
+
+    makeSuccessMsg,
+    makeFailureMsg
+
+};
