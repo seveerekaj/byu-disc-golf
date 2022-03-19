d0787a6145cd3a53e13fed91ad157c9c21480971
zhales05 <59749864+zhales05@users.noreply.github.com>
2022-03-19 16:44:15 -0600

    group working

---
+++ client/src/app/group.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
+  groupId="";
+  nickName="";
+  groupCode:any;
+  constructor() { }
+}
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Config } from 'browserslist';
+import { GroupService } from '../group.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  response: any;
+  startGame(name:string, code:string) {
+    if (code == ""){
+      this.http.post("/api/group/new-group", {"nickname" : name}).subscribe((data)=>  {
 
-  constructor() { }
+      this.response=data;
+      console.log("new", this.response.groupCode);
+
+      this.group.groupCode= this.response.groupCode;
+      this.group.groupId= this.response.groupId;
+      this.group.nickName= this.response.nickName;
+
+      this.router.navigate(["/hole/1"]);
+    })
+  } else {
+    this.http.post("/api/group/join-group", {"nickname" : name}).subscribe((data)=>  {
+      this.response=data;
+      console.log("joinGroup");
+
+      this.group.groupCode= this.response.groupCode;
+      this.group.groupId= this.response.groupId;
+      this.group.nickName= this.response.nickName;
+      
+      this.router.navigate(["/hole/1"]);
+    })
+  } 
+  }
+
+  constructor(private http:HttpClient, private router:Router, private group:GroupService) { 
+  }
 
   ngOnInit(): void {
   }
