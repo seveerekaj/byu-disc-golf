45292ffb4e33f9b80d274c64bc319c72c3a16cdb
Matt Edwards <matt.edwards@domo.com>
2022-03-19 16:09:27 -0500

    connect scoreboard and submit score to backend

--- client/src/app/scoreboard/scoreboard.component.ts
+++ client/src/app/scoreboard/scoreboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { pluck } from 'rxjs/operators';
+import { pluck, map } from 'rxjs/operators';
 import { ScoreboardWrapper } from '../scoreboard'
 
 @Component({
@@ -12,8 +12,19 @@ import { ScoreboardWrapper } from '../scoreboard'
 export class ScoreboardComponent implements OnInit {
 
   readonly SCOREBOARD_URL = '/api/group/group-id/';
-  // todo: call endpoint with actual group Id instead of hard-coded 1234
-  scoreboard$ = this.http.get<ScoreboardWrapper>(this.SCOREBOARD_URL + "1234").pipe(pluck('players'));
+  // todo: call endpoint with actual group Id isnstead of hard-coded 1234
+  scoreboard$ = this.http.get<ScoreboardWrapper>(this.SCOREBOARD_URL + "1").pipe(map(result=>{
+    const final = [];
+    for(let player of result.players){
+      const item = {
+        player: player.nickName,
+        scores: result.scoreboard[player.playerID],
+        total: result.scoreboard[player.playerID].reduce((acc,score)=>acc+score.score,0)
+      }
+      final.push(item);
+    }
+    return final;
+  }));
 
   constructor(private http: HttpClient) { }
 
