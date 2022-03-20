4e09cff867afa36f678270a04969c07af9caf685
Stephen Hales <59749864+zhales05@users.noreply.github.com>
2022-03-20 15:44:56 -0600

    Home (#23)
    * home page design
    
    * group working
    
    * remove console.log

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Config } from 'browserslist';
+import { GroupService } from '../group.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  response: any;
+  startGame(name:string, code:string) {
+    if (code == ""){
+      this.http.post("/api/group/new-group", {"nickname" : name}).subscribe((data)=>  {
+      this.response=data;
+      this.group.groupCode= this.response.groupCode;
+      this.group.groupId= this.response.groupId;
+      this.group.nickName= this.response.nickName;
 
-  constructor() { }
+      this.router.navigate(["/hole/1"]);
+    })
+  } else {
+    this.http.post("/api/group/join-group", {"nickname" : name}).subscribe((data)=>  {
+      this.response=data;
+
+      this.group.groupCode= this.response.groupCode;
+      this.group.groupId= this.response.groupId;
+      this.group.nickName= this.response.nickName;
+
+      this.router.navigate(["/hole/1"]);
+    })
+  } 
+  }
+
+  constructor(private http:HttpClient, private router:Router, private group:GroupService) { 
+  }
 
   ngOnInit(): void {
   }
