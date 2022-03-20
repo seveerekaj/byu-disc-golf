57803017dbdf06ff14f4c9b211df0b124f29f3c7
Matt Edwards <matt.edwards@domo.com>
2022-03-20 17:50:25 -0500

    persist group data so it works on refresh

--- client/src/app/group.service.ts
+++ client/src/app/group.service.ts
@@ -6,8 +6,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class GroupService {
   private groupIdSubject = new BehaviorSubject<number | null>(null);
-  private nickNameSubject = new BehaviorSubject<string>('');
-  private groupCodeSubject = new BehaviorSubject<string>('');
+  private nickNameSubject = new BehaviorSubject<string | null>('');
+  private groupCodeSubject = new BehaviorSubject<string | null>('');
   private playerIdSubject = new BehaviorSubject<number | null>(null);
 
   groupId$ = this.groupIdSubject.asObservable();
@@ -15,21 +15,39 @@ export class GroupService {
   groupCode$ = this.groupCodeSubject.asObservable();
   playerId$ = this.playerIdSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+
+    this.groupCodeSubject.next(localStorage.getItem('groupCode'));
+    this.nickNameSubject.next(localStorage.getItem('nickname'));
+    this.playerIdSubject.next(parseInt(localStorage.getItem('playerId') || ''));
+    this.groupIdSubject.next(parseInt(localStorage.getItem('groupId') || ''));
+  }
 
   setGroupId(id: number) {
     this.groupIdSubject.next(id);
+    localStorage.setItem('groupId', id.toString());
   }
 
   setGroupCode(code: string) {
     this.groupCodeSubject.next(code);
+    localStorage.setItem('groupCode', code);
   }
 
   setNickname(nickname: string) {
     this.nickNameSubject.next(nickname);
+    localStorage.setItem('nickName', nickname);
   }
 
   setPlayerId(playerId: number) {
     this.playerIdSubject.next(playerId);
+    localStorage.setItem('playerId', playerId.toString());
+  }
+
+  clearData() {
+    localStorage.clear();
+    this.groupCodeSubject.next('');
+    this.nickNameSubject.next('');
+    this.playerIdSubject.next(null);
+    this.groupIdSubject.next(null);
   }
 }
