5d74270ad38835ca89518d84d559c9b76a5be1b9
zhales05 <59749864+zhales05@users.noreply.github.com>
2022-04-04 16:51:32 -0600

    home page is now dynamic

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Config } from 'browserslist';
 import { Group } from '../group.model';
 import { GroupService } from '../group.service';
 
@@ -11,6 +10,27 @@ import { GroupService } from '../group.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  groupGame = false;
+  buttonText="Start a Game";
+  subButtonText = "Join a Group Game";
+  img = "./targets-header.jpg";
+
+  setGroupGame(){
+    this.groupGame = !this.groupGame;
+    if (this.buttonText == "Start a Game") {
+      this.buttonText = "Join Game";
+    } else {
+      this.buttonText = "Start a Game";
+    }
+
+    if (this.subButtonText == "Join a Group Game") {
+      this.subButtonText = "Start a Game";
+    } else {
+      this.subButtonText = "Join a Group Game";
+    }
+  
+  }
+
   startGame(name: string, code: string) {
     const responseHandler = (data: Group) => {
       console.log(data);
